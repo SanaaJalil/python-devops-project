32605586210f049756efa2af6b269208bf4577a7
SanaaJalil <sanaaabdullah21@gmail.com>
2025-05-13 20:14:49 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Addition of 3 + 4 =", add(3, 4))
+    print("Multiplication of 3 * 4 =", multiply(3, 4))
